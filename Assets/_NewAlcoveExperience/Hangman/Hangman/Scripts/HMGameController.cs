915588e5316dc1a2ca34696b5c1a018db550ade0
Stephanie Dimitrova <antoan@mit.edu>
2020-06-14 22:01:57 +0300

    updated

--- Assets/_NewAlcoveExperience/Hangman/Hangman/Scripts/HMGameController.cs
+++ Assets/_NewAlcoveExperience/Hangman/Hangman/Scripts/HMGameController.cs
@@ -19,7 +19,8 @@ public class HMGameController:MonoBehaviour{
 
     public GameObject[] hangmanStates;				// Manually assing states in the scene of the different states of the hanged man
     public GameObject hangmanWin;					// Win screen game object
-    public HMTopics[] topics;						// Manually assign topics with words to this array
+    public HMTopics[] topics;
+    public GameObject word; // Manually assign topics with words to this array
     
     int currentHangmanState;		// The current index of hangmanState (game object on stage)
     string currentTopic;			// Current topic name, used for the topic text
@@ -52,38 +53,44 @@ public class HMGameController:MonoBehaviour{
 
      void Update()
     {
-        Debug.Log(currentWord);
+      //  Debug.Log(currentWord);
+        if (PhotonNetwork.isMasterClient) UISolveText.text = "The word is " + currentWord;
     }
 
-    public void Start() {
-    	UINextButton.interactable = false;			// Disable the restart button
-    	UISolutionText.text = "";                   // Wipe out the solution text
-        StartCoroutine(TopicFinder());							// Find a random topic from the topic array
-    	words = topics[currentTopicIndex].words.Split(topics[currentTopicIndex].splitCharacter[0]);     // Split the words from the topics into a array of strings
-        StartCoroutine(LoadWord());								// Find a random word from the words array
-        //GetWord();    	
-        StartCoroutine(WordText());				// Delayed part of the start so that the keyboard script has finished adding letters to allKeyboardLetters variable
+    public void Start()
+    {
+        UINextButton.interactable = false;          // Disable the restart button
+        UISolutionText.text = "";                   // Wipe out the solution text
+        if (PhotonNetwork.isMasterClient)
+        {
+            StartCoroutine(TopicFinder());                          // Find a random topic from the topic array
+                                                                    // Split the words from the topics into a array of strings
+            StartCoroutine(LoadWord());                             // Find a random word from the words array
+                                                                    //GetWord();    	
+            StartCoroutine(WordText());
+            // Delayed part of the start so that the keyboard script has finished adding letters to allKeyboardLetters variable
+        }
     }
 
     private IEnumerator LoadWord()
     {
         yield return new WaitForSeconds(6.0f);
         photonView = GetComponent<PhotonView>();
-        photonView.RPC("GetWord", PhotonTargets.MasterClient);
+        photonView.RPC("GetWord", PhotonTargets.OthersBuffered);
     }
 
     private IEnumerator WordText()
     {
         yield return new WaitForSeconds(6.2f);
         photonView1 = GetComponent<PhotonView>();
-        photonView1.RPC("LateStart", PhotonTargets.MasterClient);
+        photonView1.RPC("LateStart", PhotonTargets.OthersBuffered);
     }
 
     private IEnumerator TopicFinder()
     {
         yield return new WaitForSeconds(5.8f);
         photonView2 = GetComponent<PhotonView>();
-        photonView2.RPC("RandomTopic", PhotonTargets.MasterClient);
+        photonView2.RPC("RandomTopic", PhotonTargets.OthersBuffered);
     }
     [PunRPC]
     public void LateStart(){
@@ -110,11 +117,11 @@ public class HMGameController:MonoBehaviour{
     		UISolveText.text = n;								// Change the solve word text in the UI
     		if(lettersFound >= currentWordLength){              // Check if the complete word has been solved
                 photonView3 = GetComponent<PhotonView>();
-                photonView3.RPC("SolvedWord", PhotonTargets.MasterClient);                              // The word has been solved
+                photonView3.RPC("SolvedWord", PhotonTargets.OthersBuffered);                              // The word has been solved
             }
     	}else{
             photonView4 = GetComponent<PhotonView>();
-            photonView4.RPC("WrongLetter", PhotonTargets.MasterClient);
+            photonView4.RPC("WrongLetter", PhotonTargets.OthersBuffered);
 			// No letters has been solved
     	}
     	return foundLetter;										// Return true or false, used to change letter button colors (red if wrong)
@@ -143,10 +150,20 @@ public class HMGameController:MonoBehaviour{
     }
     [PunRPC]
     public void GetWord(){
-    	 currentWord = words[UnityEngine.Random.Range(0, words.Length)].ToUpper();
+        words = topics[currentTopicIndex].words.Split(topics[currentTopicIndex].splitCharacter[0]);
+        currentWord = words[UnityEngine.Random.Range(0, words.Length)].ToUpper();
+       // word = PhotonNetwork.Instantiate(currentWord, new Vector3(0, 0, 0), Quaternion.identity, 1);
+        photonView.RPC("SendString", PhotonTargets.MasterClient, currentWord);      
         // Find a random word from the words array
     }
-    
+
+    [PunRPC]
+    public void SendString(String currentWord)
+    {
+        String recieved_string = currentWord;
+        Debug.Log(recieved_string);
+    }
+
     public int CountLettersInWord(){										//Counts letters in the current word, only counts letters that is represented in the keyboard
     	int c = currentWord.Length;
     	int s = 0;
